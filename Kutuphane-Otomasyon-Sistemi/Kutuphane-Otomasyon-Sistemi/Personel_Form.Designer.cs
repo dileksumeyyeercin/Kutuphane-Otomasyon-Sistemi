8c9f41faae8605fc663306d05ee806c44b7b3dbb
fatmauralgul <fatmanuralgull@gmail.com>
2022-05-26 12:56:52 +0300

    personel kısmı tamamlandı

--- Kutuphane-Otomasyon-Sistemi/Kutuphane-Otomasyon-Sistemi/Personel_Form.Designer.cs
+++ Kutuphane-Otomasyon-Sistemi/Kutuphane-Otomasyon-Sistemi/Personel_Form.Designer.cs
@@ -29,12 +29,196 @@ namespace Kutuphane_Otomasyon_Sistemi
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnGüncelle = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.txtKadi = new System.Windows.Forms.TextBox();
+            this.txtTelefon = new System.Windows.Forms.TextBox();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(-6, 325);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(945, 228);
+            this.dataGridView1.TabIndex = 29;
+            this.dataGridView1.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEnter);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(462, 217);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(127, 40);
+            this.btnSil.TabIndex = 28;
+            this.btnSil.Text = "Kullanıcı Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnGüncelle
+            // 
+            this.btnGüncelle.Location = new System.Drawing.Point(462, 147);
+            this.btnGüncelle.Name = "btnGüncelle";
+            this.btnGüncelle.Size = new System.Drawing.Size(127, 40);
+            this.btnGüncelle.TabIndex = 27;
+            this.btnGüncelle.Text = "Kullanıcı Güncelle";
+            this.btnGüncelle.UseVisualStyleBackColor = true;
+            this.btnGüncelle.Click += new System.EventHandler(this.btnGüncelle_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(84, 253);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 26;
+            this.label5.Text = "Şifre:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(84, 207);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "Kullanıcı Adı:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(84, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "Telefon No:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(84, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "Soyadı:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(84, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Adı:";
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(462, 82);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(127, 40);
+            this.btnEkle.TabIndex = 21;
+            this.btnEkle.Text = "Kullanıcı Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.Location = new System.Drawing.Point(180, 239);
+            this.txtSifre.Multiline = true;
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.PasswordChar = '*';
+            this.txtSifre.Size = new System.Drawing.Size(122, 29);
+            this.txtSifre.TabIndex = 20;
+            // 
+            // txtKadi
+            // 
+            this.txtKadi.Location = new System.Drawing.Point(180, 193);
+            this.txtKadi.Multiline = true;
+            this.txtKadi.Name = "txtKadi";
+            this.txtKadi.Size = new System.Drawing.Size(122, 29);
+            this.txtKadi.TabIndex = 19;
+            // 
+            // txtTelefon
+            // 
+            this.txtTelefon.Location = new System.Drawing.Point(180, 147);
+            this.txtTelefon.Multiline = true;
+            this.txtTelefon.Name = "txtTelefon";
+            this.txtTelefon.Size = new System.Drawing.Size(122, 29);
+            this.txtTelefon.TabIndex = 18;
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(180, 106);
+            this.txtSoyad.Multiline = true;
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(122, 29);
+            this.txtSoyad.TabIndex = 17;
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(180, 63);
+            this.txtAd.Multiline = true;
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(122, 29);
+            this.txtAd.TabIndex = 16;
+            // 
+            // Personel_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(951, 647);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnGüncelle);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.txtSifre);
+            this.Controls.Add(this.txtKadi);
+            this.Controls.Add(this.txtTelefon);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.txtAd);
+            this.Name = "Personel_Form";
             this.Text = "Personel_Form";
+            this.Load += new System.EventHandler(this.Personel_Form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnGüncelle;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.TextBox txtSifre;
+        private System.Windows.Forms.TextBox txtKadi;
+        private System.Windows.Forms.TextBox txtTelefon;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.TextBox txtAd;
     }
 }
--- Kutuphane-Otomasyon-Sistemi/Kutuphane-Otomasyon-Sistemi/Personel_Form.cs
+++ Kutuphane-Otomasyon-Sistemi/Kutuphane-Otomasyon-Sistemi/Personel_Form.cs
@@ -7,14 +7,87 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Data.SqlClient; 
 
 namespace Kutuphane_Otomasyon_Sistemi
 {
     public partial class Personel_Form : Form
     {
+        MySqlConnection baglanti;
+        MySqlCommand komut;
+        MySqlDataAdapter da;
         public Personel_Form()
         {
             InitializeComponent();
+        }
+        void PersonelGetir()
+        {
+            baglanti = new MySqlConnection("SERVER=172.21.54.3;DATABASE=Lunatic;UID=Lunatic;password=B.batur1316");
+            baglanti.Open();
+            da = new MySqlDataAdapter("SELECT *FROM personel ", baglanti);
+            DataTable tablo = new DataTable();
+            da.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+            baglanti.Close();
+        }
+
+        private void Personel_Form_Load(object sender, EventArgs e)
+        {
+            PersonelGetir();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            string sorgu = ("INSERT INTO personel (adi,soyadi,telefon,kullanici_adi,sifre) VALUES (@adi,@soyadi,@telefon,@kullanici_adi,@sifre)");
+            komut = new MySqlCommand(sorgu, baglanti);
+            komut.Parameters.AddWithValue("@adi", txtAd.Text);
+            komut.Parameters.AddWithValue("@soyadi", txtSoyad.Text);
+            komut.Parameters.AddWithValue("@telefon", txtTelefon.Text);
+            komut.Parameters.AddWithValue("@kullanici_adi", txtKadi.Text);
+            komut.Parameters.AddWithValue("@sifre", txtSifre.Text);
+            baglanti.Open();
+            MessageBox.Show("Kullanıcı Eklendi.");
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            PersonelGetir();
+        }
+
+        private void btnGüncelle_Click(object sender, EventArgs e)
+        {
+            string sorgu = "UPDATE personel SET adi=@adi,soyadi=@soyadi,telefon=@telefon,sifre=@sifre WHERE kullanici_adi=@kullanici_adi";
+            komut = new MySqlCommand(sorgu, baglanti);
+            komut.Parameters.AddWithValue("@kullanici_adi", Convert.ToString(txtKadi.Text));
+            komut.Parameters.AddWithValue("@adi", txtAd.Text);
+            komut.Parameters.AddWithValue("@soyadi", txtSoyad.Text);
+            komut.Parameters.AddWithValue("@telefon", txtTelefon.Text);
+            komut.Parameters.AddWithValue("@sifre", txtSifre.Text);
+            baglanti.Open();
+            MessageBox.Show("Kullanıcı Güncellendi.");
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            PersonelGetir();
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            string sorgu = ("DELETE FROM personel WHERE kullanici_adi=@kullanici_adi");
+            komut = new MySqlCommand(sorgu, baglanti);
+            komut.Parameters.AddWithValue("@kullanici_adi", Convert.ToString(txtKadi.Text));
+            baglanti.Open();
+            MessageBox.Show("Kullanıcı Silindi.");
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            PersonelGetir();
+        }
+
+        private void dataGridView1_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            txtAd.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            txtSoyad.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            txtTelefon.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            txtKadi.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+            txtSifre.Text = dataGridView1.CurrentRow.Cells[5].Value.ToString();
         }
     }
 }
