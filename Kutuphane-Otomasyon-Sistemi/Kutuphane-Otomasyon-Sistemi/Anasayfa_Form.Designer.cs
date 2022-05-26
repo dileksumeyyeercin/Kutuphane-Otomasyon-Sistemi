0d1a3bb2bd6bf4a1ca3ec2e31e15d22ceb7347dd
barinbatur13 <barinlunatic@outlook.com>
2022-05-26 12:25:20 +0300

    anasayfa tamamlandı

--- Kutuphane-Otomasyon-Sistemi/Kutuphane-Otomasyon-Sistemi/Anasayfa_Form.Designer.cs
+++ Kutuphane-Otomasyon-Sistemi/Kutuphane-Otomasyon-Sistemi/Anasayfa_Form.Designer.cs
@@ -29,12 +29,141 @@ namespace Kutuphane_Otomasyon_Sistemi
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Anasayfa_Form));
+            this.btnKitaplar = new System.Windows.Forms.Button();
+            this.btnUye = new System.Windows.Forms.Button();
+            this.btnTeslim = new System.Windows.Forms.Button();
+            this.btnEmanet = new System.Windows.Forms.Button();
+            this.btnPersonelKayit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnKitaplar
+            // 
+            this.btnKitaplar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnKitaplar.BackgroundImage")));
+            this.btnKitaplar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnKitaplar.Font = new System.Drawing.Font("Bookman Old Style", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKitaplar.Location = new System.Drawing.Point(190, 204);
+            this.btnKitaplar.Name = "btnKitaplar";
+            this.btnKitaplar.Size = new System.Drawing.Size(185, 138);
+            this.btnKitaplar.TabIndex = 10;
+            this.btnKitaplar.Text = "KİTAPLAR";
+            this.btnKitaplar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnKitaplar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnKitaplar.UseVisualStyleBackColor = true;
+            this.btnKitaplar.Click += new System.EventHandler(this.btnKitaplar_Click);
+            // 
+            // btnUye
+            // 
+            this.btnUye.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnUye.BackgroundImage")));
+            this.btnUye.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnUye.Font = new System.Drawing.Font("Bookman Old Style", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnUye.Location = new System.Drawing.Point(468, 204);
+            this.btnUye.Name = "btnUye";
+            this.btnUye.Size = new System.Drawing.Size(185, 138);
+            this.btnUye.TabIndex = 15;
+            this.btnUye.Text = "ÜYELER";
+            this.btnUye.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnUye.UseVisualStyleBackColor = true;
+            this.btnUye.Click += new System.EventHandler(this.btnUye_Click);
+            // 
+            // btnTeslim
+            // 
+            this.btnTeslim.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnTeslim.BackgroundImage")));
+            this.btnTeslim.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnTeslim.Font = new System.Drawing.Font("Bookman Old Style", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnTeslim.Location = new System.Drawing.Point(741, 204);
+            this.btnTeslim.Name = "btnTeslim";
+            this.btnTeslim.Size = new System.Drawing.Size(185, 138);
+            this.btnTeslim.TabIndex = 16;
+            this.btnTeslim.Text = "TESLİM İŞLEMLERİ";
+            this.btnTeslim.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnTeslim.UseVisualStyleBackColor = true;
+            this.btnTeslim.Click += new System.EventHandler(this.btnTeslim_Click);
+            // 
+            // btnEmanet
+            // 
+            this.btnEmanet.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnEmanet.BackgroundImage")));
+            this.btnEmanet.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnEmanet.Font = new System.Drawing.Font("Bookman Old Style", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEmanet.Location = new System.Drawing.Point(321, 390);
+            this.btnEmanet.Name = "btnEmanet";
+            this.btnEmanet.Size = new System.Drawing.Size(185, 142);
+            this.btnEmanet.TabIndex = 17;
+            this.btnEmanet.Text = "EMANET KİTAPLAR";
+            this.btnEmanet.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnEmanet.UseVisualStyleBackColor = true;
+            this.btnEmanet.Click += new System.EventHandler(this.btnEmanet_Click);
+            // 
+            // btnPersonelKayit
+            // 
+            this.btnPersonelKayit.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnPersonelKayit.BackgroundImage")));
+            this.btnPersonelKayit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnPersonelKayit.Font = new System.Drawing.Font("Bookman Old Style", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnPersonelKayit.Location = new System.Drawing.Point(620, 390);
+            this.btnPersonelKayit.Name = "btnPersonelKayit";
+            this.btnPersonelKayit.Size = new System.Drawing.Size(185, 142);
+            this.btnPersonelKayit.TabIndex = 20;
+            this.btnPersonelKayit.Text = "PERSONEL İŞLEMLERİ";
+            this.btnPersonelKayit.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnPersonelKayit.UseVisualStyleBackColor = true;
+            this.btnPersonelKayit.Click += new System.EventHandler(this.btnPersonelKayit_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(87, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(917, 73);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "ULUDAĞ ÜNİVERSİTESİ\r\n İNEGÖL İŞLETME FAKÜLTESİ KÜTÜPHANE OTOMASYON SİSTEMİ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(478, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(149, 103);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 23;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Anasayfa_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(232)))), ((int)(((byte)(254)))));
+            this.ClientSize = new System.Drawing.Size(1116, 544);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnPersonelKayit);
+            this.Controls.Add(this.btnEmanet);
+            this.Controls.Add(this.btnTeslim);
+            this.Controls.Add(this.btnUye);
+            this.Controls.Add(this.btnKitaplar);
+            this.Font = new System.Drawing.Font("Bookman Old Style", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Anasayfa_Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Anasayfa_Form";
+            this.Load += new System.EventHandler(this.Anasayfa_Form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnKitaplar;
+        private System.Windows.Forms.Button btnUye;
+        private System.Windows.Forms.Button btnTeslim;
+        private System.Windows.Forms.Button btnEmanet;
+        private System.Windows.Forms.Button btnPersonelKayit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- Kutuphane-Otomasyon-Sistemi/Kutuphane-Otomasyon-Sistemi/Anasayfa_Form.cs
+++ Kutuphane-Otomasyon-Sistemi/Kutuphane-Otomasyon-Sistemi/Anasayfa_Form.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Kutuphane_Otomasyon_Sistemi
 {
@@ -15,6 +16,41 @@ namespace Kutuphane_Otomasyon_Sistemi
         public Anasayfa_Form()
         {
             InitializeComponent();
+        }
+
+        private void Anasayfa_Form_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnKitaplar_Click(object sender, EventArgs e)
+        {
+            Kitaplar_Form kitaplar = new Kitaplar_Form();
+            kitaplar.ShowDialog();
+        }
+
+        private void btnUye_Click(object sender, EventArgs e)
+        {
+            Uyeler_Form uyeler = new Uyeler_Form();
+            uyeler.ShowDialog();
+        }
+
+        private void btnTeslim_Click(object sender, EventArgs e)
+        {
+            TeslimAlma_Form teslim = new TeslimAlma_Form();
+            teslim.ShowDialog();
+        }
+
+        private void btnEmanet_Click(object sender, EventArgs e)
+        {
+            EmanetKitaplar_Form emanetKitaplar = new EmanetKitaplar_Form();
+            emanetKitaplar.ShowDialog();
+        }
+
+        private void btnPersonelKayit_Click(object sender, EventArgs e)
+        {
+            Personel_Form personelForm = new Personel_Form();
+            personelForm.ShowDialog();
         }
     }
 }
