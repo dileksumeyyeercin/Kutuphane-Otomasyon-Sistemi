5dd8ee6a2107e2764372b221c3867c4438922ea6
fatmauralgul <fatmanuralgull@gmail.com>
2022-05-27 15:39:44 +0300

    kategoriler tamamlandı

---
+++ Kutuphane-Otomasyon-Sistemi/Kutuphane-Otomasyon-Sistemi/Kategoriler_Form.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace Kutuphane_Otomasyon_Sistemi
+{
+    partial class Kategoriler_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAdi = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtAdi
+            // 
+            this.txtAdi.BackColor = System.Drawing.Color.White;
+            this.txtAdi.Location = new System.Drawing.Point(280, 71);
+            this.txtAdi.Multiline = true;
+            this.txtAdi.Name = "txtAdi";
+            this.txtAdi.Size = new System.Drawing.Size(167, 31);
+            this.txtAdi.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(221, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Adı:";
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(103)))), ((int)(((byte)(155)))), ((int)(((byte)(241)))));
+            this.btnEkle.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEkle.Location = new System.Drawing.Point(359, 323);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(88, 31);
+            this.btnEkle.TabIndex = 3;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = false;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(103)))), ((int)(((byte)(155)))), ((int)(((byte)(241)))));
+            this.btnSil.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSil.Location = new System.Drawing.Point(499, 323);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(88, 31);
+            this.btnSil.TabIndex = 4;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = false;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(225, 130);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(362, 150);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // Kategoriler_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(232)))), ((int)(((byte)(254)))));
+            this.ClientSize = new System.Drawing.Size(729, 439);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAdi);
+            this.Name = "Kategoriler_Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kategoriler_Form";
+            this.Load += new System.EventHandler(this.Kategoriler_Form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAdi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ Kutuphane-Otomasyon-Sistemi/Kutuphane-Otomasyon-Sistemi/Kategoriler_Form.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+namespace Kutuphane_Otomasyon_Sistemi
+{
+    public partial class Kategoriler_Form : Form
+    {
+
+        MySqlConnection baglanti;
+        MySqlCommand komut;
+        MySqlDataAdapter da;
+        public Kategoriler_Form()
+        {
+            InitializeComponent();
+        }
+        void KategoriGetir()
+        {
+            baglanti = new MySqlConnection("SERVER=172.21.54.3;DATABASE=Lunatic;UID=Lunatic;password=B.batur1316");
+            baglanti.Open();
+            da = new MySqlDataAdapter("SELECT *FROM kategori ", baglanti);
+            DataTable tablo = new DataTable();
+            da.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+            baglanti.Close();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            string sorgu = ("INSERT INTO kategori (adi) VALUES (@adi)");
+            komut = new MySqlCommand(sorgu, baglanti);
+            komut.Parameters.AddWithValue("@adi", txtAdi.Text);
+            baglanti.Open();
+            MessageBox.Show(" Eklendi.");
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            KategoriGetir();
+        }
+
+        private void Kategoriler_Form_Load(object sender, EventArgs e)
+        {
+            KategoriGetir();
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            string sorgu = ("DELETE FROM kategori WHERE adi=@adi");
+            komut = new MySqlCommand(sorgu, baglanti);
+            komut.Parameters.AddWithValue("@adi", (txtAdi.Text));
+            baglanti.Open();
+            MessageBox.Show(" Silindi.");
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            KategoriGetir();
+        }
+
+        private void dataGridView1_CellBeginEdit(object sender, DataGridViewCellCancelEventArgs e)
+        {
+
+        }
+    }
+}
