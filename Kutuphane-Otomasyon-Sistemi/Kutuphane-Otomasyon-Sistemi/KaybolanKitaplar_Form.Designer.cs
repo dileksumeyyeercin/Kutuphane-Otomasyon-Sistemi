5999e1f4a87a69633425a9f9b211db71475e3fd9
mecitataseverr <mecitats47@gmail.com>
2022-06-02 02:48:24 +0300

    kayıp kitap bölümü eklendi

--- Kutuphane-Otomasyon-Sistemi/Kutuphane-Otomasyon-Sistemi/KaybolanKitaplar_Form.Designer.cs
+++ Kutuphane-Otomasyon-Sistemi/Kutuphane-Otomasyon-Sistemi/KaybolanKitaplar_Form.Designer.cs
@@ -29,20 +29,267 @@ namespace Kutuphane_Otomasyon_Sistemi
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtKitapAra = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column11 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column12 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column9});
+            this.dataGridView1.Location = new System.Drawing.Point(315, 39);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.ShowEditingIcon = false;
+            this.dataGridView1.Size = new System.Drawing.Size(488, 203);
+            this.dataGridView1.TabIndex = 38;
+            // 
+            // Column1
+            // 
+            this.Column1.DataPropertyName = "id";
+            this.Column1.HeaderText = "#";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            this.Column2.DataPropertyName = "adi";
+            this.Column2.HeaderText = "Adı";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 125;
+            // 
+            // Column3
+            // 
+            this.Column3.DataPropertyName = "sayfa_sayisi";
+            this.Column3.HeaderText = "Sayfa Sayısı";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 125;
+            // 
+            // Column4
+            // 
+            this.Column4.DataPropertyName = "tur";
+            this.Column4.HeaderText = "Tür";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            this.Column4.Width = 125;
+            // 
+            // Column5
+            // 
+            this.Column5.DataPropertyName = "demirBas_no";
+            this.Column5.HeaderText = "Demirbaş No";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            this.Column5.Width = 125;
+            // 
+            // Column6
+            // 
+            this.Column6.DataPropertyName = "yayinevi";
+            this.Column6.HeaderText = "Yayınevi";
+            this.Column6.MinimumWidth = 6;
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            this.Column6.Width = 125;
+            // 
+            // Column7
+            // 
+            this.Column7.DataPropertyName = "adi_soyadi";
+            this.Column7.HeaderText = "Yazar";
+            this.Column7.MinimumWidth = 6;
+            this.Column7.Name = "Column7";
+            this.Column7.ReadOnly = true;
+            this.Column7.Width = 125;
+            // 
+            // Column8
+            // 
+            this.Column8.DataPropertyName = "adi1";
+            this.Column8.HeaderText = "Kategori";
+            this.Column8.MinimumWidth = 6;
+            this.Column8.Name = "Column8";
+            this.Column8.ReadOnly = true;
+            this.Column8.Width = 125;
+            // 
+            // Column9
+            // 
+            this.Column9.DataPropertyName = "raf";
+            this.Column9.HeaderText = "Raf";
+            this.Column9.MinimumWidth = 6;
+            this.Column9.Name = "Column9";
+            this.Column9.ReadOnly = true;
+            this.Column9.Width = 125;
+            // 
+            // txtKitapAra
+            // 
+            this.txtKitapAra.Location = new System.Drawing.Point(345, 13);
+            this.txtKitapAra.Name = "txtKitapAra";
+            this.txtKitapAra.Size = new System.Drawing.Size(100, 20);
+            this.txtKitapAra.TabIndex = 39;
+            this.txtKitapAra.TextChanged += new System.EventHandler(this.txtKitapAra_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(261, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 40;
+            this.label1.Text = "Kitap Ara";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.AllowUserToResizeColumns = false;
+            this.dataGridView2.AllowUserToResizeRows = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column10,
+            this.Column11,
+            this.Column12});
+            this.dataGridView2.Location = new System.Drawing.Point(315, 289);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.RowHeadersVisible = false;
+            this.dataGridView2.ShowEditingIcon = false;
+            this.dataGridView2.Size = new System.Drawing.Size(488, 185);
+            this.dataGridView2.TabIndex = 41;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(847, 109);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 23);
+            this.button1.TabIndex = 42;
+            this.button1.Text = "Kayıp Kitap Ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(847, 148);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(177, 58);
+            this.button2.TabIndex = 43;
+            this.button2.Text = "Kayıp Kitap Düzelt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(905, 298);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 44;
+            this.button3.Text = "Sil";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Column10
+            // 
+            this.Column10.DataPropertyName = "id";
+            this.Column10.HeaderText = "#";
+            this.Column10.Name = "Column10";
+            this.Column10.ReadOnly = true;
+            // 
+            // Column11
+            // 
+            this.Column11.DataPropertyName = "adi";
+            this.Column11.HeaderText = "Kitap Adı";
+            this.Column11.Name = "Column11";
+            this.Column11.ReadOnly = true;
+            // 
+            // Column12
+            // 
+            this.Column12.DataPropertyName = "kayipdurum";
+            this.Column12.HeaderText = "Kayıp Durum";
+            this.Column12.Name = "Column12";
+            this.Column12.ReadOnly = true;
             // 
             // KaybolanKitaplar_Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1265, 556);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtKitapAra);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "KaybolanKitaplar_Form";
             this.Text = "KaybolanKitaplar_Form";
             this.Load += new System.EventHandler(this.KaybolanKitaplar_Form_Load);
+            this.Shown += new System.EventHandler(this.KaybolanKitaplar_Form_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.TextBox txtKitapAra;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column10;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column11;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column12;
     }
 }
--- Kutuphane-Otomasyon-Sistemi/Kutuphane-Otomasyon-Sistemi/KaybolanKitaplar_Form.cs
+++ Kutuphane-Otomasyon-Sistemi/Kutuphane-Otomasyon-Sistemi/KaybolanKitaplar_Form.cs
@@ -20,9 +20,127 @@ namespace Kutuphane_Otomasyon_Sistemi
         Kitaplar_Form frm = new Kitaplar_Form();
         MySqlConnection connection = new MySqlConnection("SERVER=172.21.54.3;DATABASE=Lunatic;UID=Lunatic;password=B.batur1316");
 
+        DataSet daset = new DataSet();
         private void KaybolanKitaplar_Form_Load(object sender, EventArgs e)
         {
             
+        }
+        private void araVeGoster(string sorgu, DataGridView dgv)
+        {
+            string sql = sorgu;
+            MySqlConnection con = GetConnection();
+            MySqlCommand cmd = new MySqlCommand(sql, con);
+            MySqlDataAdapter adp = new MySqlDataAdapter(cmd);
+            DataTable tbl = new DataTable();
+            adp.Fill(tbl);
+            dgv.DataSource = tbl;
+            con.Close();
+
+        }
+        public static MySqlConnection GetConnection()
+        {
+            string sql = "SERVER=172.21.54.3;DATABASE=Lunatic;UID=Lunatic;password=B.batur1316";
+            MySqlConnection con = new MySqlConnection(sql);
+            try
+            {
+                con.Open();
+
+            }
+            catch (MySqlException excep)
+            {
+                MessageBox.Show("MySqlConnection!! \n" + excep.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return con;
+        }
+        public void kayipListele()
+        {
+            araVeGoster("SELECT kayip.id,kitapLar.adi,kayip.kayipdurum FROM kayip INNER JOİN kitapLar ON  kayip.kitap_id=kitapLar.id ", dataGridView2);
+        }
+        public void Kitap_Listele()
+        {
+            connection.Open();
+            MySqlDataAdapter adtr = new MySqlDataAdapter("SELECT kitapLar.id,kitapLar.adi,kitapLar.sayfa_sayisi,kitapLar.tur,kitapLar.demirBas_no,kitapLar.yayinevi,yazarlar.adi_soyadi,kategori.adi,kitapLar.raf FROM kitapLar INNER JOİN kategori ON kitapLar.kategori_id = kategori.id INNER JOİN  yazarlar  ON kitapLar.yazar_id = yazarlar.id", connection);
+            adtr.Fill(daset, "kitapLar");
+            dataGridView1.DataSource = daset.Tables["kitapLar"];
+            connection.Close();
+        }
+        private void txtKitapAra_TextChanged(object sender, EventArgs e)
+        {
+            daset.Tables["kitapLar"].Clear();
+            connection.Open();
+            MySqlDataAdapter adtr = new MySqlDataAdapter("select * from kitapLar where adi like '%" + txtKitapAra.Text + "%'", connection);
+            adtr.Fill(daset, "kitapLar");
+            dataGridView1.DataSource = daset.Tables["kitapLar"];
+            connection.Close();
+        }
+
+        private void KaybolanKitaplar_Form_Shown(object sender, EventArgs e)
+        {
+            Kitap_Listele();
+            kayipListele();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MySqlConnection con = new MySqlConnection("SERVER=172.21.54.3;DATABASE=Lunatic;UID=Lunatic;password=B.batur1316");
+            con.Open();
+            MySqlCommand cmd = new MySqlCommand("INSERT INTO kayip(kayip.kitap_id) VALUES (@kitap_id)",con);
+            cmd.Parameters.AddWithValue("@kitap_id",dataGridView1.CurrentRow.Cells[0].Value);
+            try
+            {
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Kayıp Kitap Olarak Belirlendi");
+                kayipListele();
+            }
+            catch (Exception hata)
+            {
+
+                MessageBox.Show("Kayıp kitap tablosuna eklenmedi" + hata.Message);
+            }
+            con.Close();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MySqlConnection con = new MySqlConnection("SERVER=172.21.54.3;DATABASE=Lunatic;UID=Lunatic;password=B.batur1316");
+            string sorgu = "UPDATE kayip SET kayipdurum='Kitap Bulundu' WHERE id =@id";
+            con.Open();
+            MySqlCommand command = new MySqlCommand(sorgu, con);
+            command.Parameters.AddWithValue("@id", dataGridView2.CurrentRow.Cells[0].Value);
+            try
+            {
+                command.ExecuteNonQuery();
+                MessageBox.Show("Kitap Bulundu");
+                kayipListele();
+            }
+            catch (Exception msj)
+            {
+                MessageBox.Show("Kitap geri alma başarısız " + msj.Message);
+
+            }
+            con.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string sql = "DELETE FROM kayip WHERE id = @id";
+            MySqlConnection con = GetConnection();
+            MySqlCommand cmd = new MySqlCommand(sql, con);
+            cmd.Parameters.AddWithValue("@id", dataGridView2.CurrentRow.Cells[0].Value);
+            try
+            {
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Kayıp Kitap Veri Tabanından Silindi");
+                kayipListele();
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Hata" + ex.Message);
+            }
+            con.Close();
         }
     }
 }
